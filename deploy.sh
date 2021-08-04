b2e98d523001af8b5901ef3bc62cfe2a2a7cbd29
hotarugali <hotarugali@163.com>
2021-08-04 17:25:43 +0800

    Wed Aug  4 05:25:43 PM CST 2021

---
+++ deploy.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+function Exit{
+    exit
+}
+
+function Usage{
+    echo "Usage: $(basename $0) <option>"
+    echo 
+    echo "至少需要指定以下一个选项："
+    echo "  -a,--all                : 处理所有表情包目录。"
+    echo "  -d,--dir <directory>    : 处理单个表情包目录。"
+    echo "  -h,--help               : 显示帮助信息。"
+    Exit
+}
+
+
+function Branch{
+    git branch "$_name_"
+    git checkout "$_name_"
+    git rm --cached -r .
+    git clean -f -d
+    git checkout master
+    git checkout "$_name_" "$_dir_"
+}
+
+
+if [ "$#" -lt 1 ] || [ "$#" -gt 3 ]; then
+    echo "Usage Error!"
+    Usage
+else
+    for (( i=1; i<=$#; ++i )) do
+		case "${!i}" in
+			"-a" | "--all")
+				for i in image/* ; do
+                    _dir_="$i"
+                    _name_="$(basename $i)"
+                    Branch
+                done
+                Exit
+				;;
+			"-d" | "--dir")
+				((++i))
+				_dir_="${!i}"
+                _name_="$(basename $i)"
+                Branch
+                Exit
+				;;
+			"-h" | "--help")
+				((++i))
+				Usage
+				;;
+			*)
+				echo "Usage Error"
+				echo "Invalid option: ${!i}"
+				Usage
+				;;
+		esac
+	done
+fi
--- py/waline.py
+++ py/waline.py
@@ -1,6 +1,7 @@
 import os
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 import json
+import argparse
 
 IMAGE_SUFFIX = ['.png', '.jpg', 'jpeg', '.gif', '.webp']
 
@@ -8,7 +9,7 @@ IMAGE_SUFFIX = ['.png', '.jpg', 'jpeg', '.gif', '.webp']
 def scanDir(repo, dir, cdn):
     info = {}
     info['name'] = os.path.basename(dir)
-    info['folder'] = os.path.join(cdn, dir.split(f'/{repo}/')[1].strip("/"))
+    info['folder'] = os.path.join(cdn, f"{repo}@{info['name']}", dir.split(f'/{repo}/')[1].strip("/"))
     info['items'] = []
     for file in sorted(os.listdir(dir)):
         if os.path.splitext(file)[-1].lower() in IMAGE_SUFFIX:
@@ -17,17 +18,42 @@ def scanDir(repo, dir, cdn):
     info['icon'] = info['items'][0]
     return info
 
+
 if __name__=='__main__':
+    # 读取命令行参数
+    parse = argparse.ArgumentParser()
+    group = parse.add_mutually_exclusive_group(required=True)
+    group.add_argument('--all', '-a', action='store_true', default=False, help='是否同时处理所有表情包目录。')
+    group.add_argument('--dir', '-d', type=str, default=None, help='指定单个表情包目录进行处理。')
+    args = parse.parse_args()
+
+    
+    # 处理表情包
     repo = 'Emoji'
     path = os.getcwd()
+
     image = os.path.normpath(os.path.join(path, "../image"))
     waline = os.path.normpath(os.path.join(path, "../waline"))
-    emojiCDN = 'https://hotarugali.github.io/Emoji/'
-    for file in os.listdir(image):
-        dir = os.path.join(image, file)
-        if os.path.isdir(dir):
-            info = scanDir(repo, dir, emojiCDN)
-            infoDir = os.path.join(waline, file)
-            os.makedirs(infoDir, exist_ok=True)
-            with open(os.path.join(infoDir, "info.json"), "w") as f:
-                json.dump(info, f, indent=4)
+    emojiCDN = 'https://cdn.jsdelivr.net/gh/hotarugali/'
+
+    if args.all:
+        for file in os.listdir(image):
+            dir = os.path.join(image, file)
+            if os.path.isdir(dir):
+                info = scanDir(repo, dir, emojiCDN)
+                infoDir = os.path.join(waline, file)
+                os.makedirs(infoDir, exist_ok=True)
+                with open(os.path.join(infoDir, "info.json"), "w") as f:
+                    json.dump(info, f, indent=4)
+    
+    if args.dir is not None:
+        assert os.path.exists(args.dir), "错误：路径不存在！"
+        assert os.path.isdir(args.dir), "错误：不是目录！"
+        args.dir = os.path.join(image, os.path.basename(args.dir))
+        assert os.path.exists(args.dir), "错误：表情包目录不在 '{}' 目录下！".format(image)
+
+        info = scanDir(repo, args.dir, emojiCDN)
+        infoDir = os.path.join(waline, os.path.basename(args.dir))
+        os.makedirs(infoDir, exist_ok=True)
+        with open(os.path.join(infoDir, "info.json"), "w") as f:
+            json.dump(info, f, indent=4)
