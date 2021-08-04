3817dba7c9da289f37c679d2b2b7a48a9a07e3b3
hotarugali <hotarugali@163.com>
2021-08-04 18:49:15 +0800

    Wed Aug  4 06:49:15 PM CST 2021

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-function Exit {
-    exit
-}
-
 function Usage {
     echo "Usage: $(basename $0) <option>"
     echo 
@@ -11,21 +7,19 @@ function Usage {
     echo "  -a,--all                : 处理所有表情包目录。"
     echo "  -d,--dir <directory>    : 处理单个表情包目录。"
     echo "  -h,--help               : 显示帮助信息。"
-    Exit
 }
 
-
 function Branch {
-    git branch "$_name_"
     git checkout --orphan "$_name_"
-    git rm --cached -r .
+    git rm --cached -rf .
     git clean -f -d
     git checkout master "$_dir_"
     git commit -m "$(date)"
     git checkout master
 }
 
-
+_path_=$(dirname $0)
+pushd "$_path_" > /dev/null
 if [ "$#" -lt 1 ] || [ "$#" -gt 3 ]; then
     echo "Usage Error!"
     Usage
@@ -38,14 +32,14 @@ else
                     _name_="$(basename ${!i})"
                     Branch
                 done
-                Exit
+                break
 				;;
 			"-d" | "--dir")
 				((++i))
 				_dir_="${!i}"
                 _name_="$(basename ${!i})"
                 Branch
-                Exit
+                break
 				;;
 			"-h" | "--help")
 				((++i))
@@ -59,3 +53,4 @@ else
 		esac
 	done
 fi
+popd > /dev/null
